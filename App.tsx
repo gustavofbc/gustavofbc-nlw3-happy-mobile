360b3e91f34836f58fa4572ab2b9c1028f66c661
Gustavo Felipe <gustavo.felipebc@gmail.com>
2020-10-28 19:57:47 -0300

    criaçaod o fluxo de cadastro do orfanato

--- App.tsx
+++ App.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 import { useFonts } from 'expo-font';
 import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
 
-import Routes from './scr/routes';
+
+import Routes from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, {Marker, Callout, PROVIDER_GOOGLE} from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
 
 import mapMarker from '../images/map-marker.png';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 
 export default function OrphanagesMap() {
@@ -13,6 +14,9 @@ export default function OrphanagesMap() {
 
   function handleNavigateToOrphanageDetails() {
     navigation.navigate('OrphanageDetails')
+  }
+  function handleNavigateToCreateOrphanage() {
+    navigation.navigate('SelectMapPosition')
   }
 
   return(
@@ -48,9 +52,9 @@ export default function OrphanagesMap() {
         <View style={styles.footer}>
             <Text style={styles.footerText}>2 orfanatos encontrados</Text>
         
-            <TouchableOpacity style={styles.createdOrphanageButton} onPress={() => {}}>
+            <RectButton style={styles.createdOrphanageButton} onPress={handleNavigateToCreateOrphanage}>
               <Feather name='plus' size={20} color='#FFF' />
-            </TouchableOpacity>
+            </RectButton>
         </View>
     </View>
   )
